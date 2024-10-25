b2125dba1607c93696871e2e7e2679310feb9655
SimoneMicheleAllocca <s.allocca1998@gmail.com>
2024-10-25 20:10:54 +0200

    add code c#

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("La mia prima calcolatrice con C#");
+        Console.WriteLine("Inserisci il primo numero:");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Inserisci il secondo numero:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Scegli un'operazione: +, -, *, /");
+        string operazione = Console.ReadLine();
+
+        double risultato = 0;
+
+        switch (operazione)
+        {
+            case "+":
+                risultato = num1 + num2;
+                break;
+            case "-":
+                risultato = num1 - num2;
+                break;
+            case "*":
+                risultato = num1 * num2;
+                break;
+            case "/":
+                if (num2 != 0)
+                {
+                    risultato = num1 / num2;
+                }
+                else
+                {
+                    Console.WriteLine("Errore: Divisione per zero!");
+                    return;
+                }
+                break;
+            default:
+                Console.WriteLine("Operazione non valida!");
+                return;
+        }
+
+        Console.WriteLine($"Il risultato di {num1} {operazione} {num2} è: {risultato}");
+    }
+}
