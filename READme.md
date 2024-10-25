30c48487ca0fc42daffc5b4c51367406dc6b5255
SimoneMicheleAllocca <s.allocca1998@gmail.com>
2024-10-25 20:13:13 +0200

    add READme

---
+++ READme.md
@@ -0,0 +1,11 @@
+# La mia prima calcolatrice in C#
+
+Questo progetto rappresenta la mia prima calcolatrice realizzata in C#. È un'applicazione console semplice che consente di eseguire operazioni matematiche di base.
+
+## Funzionalità
+
+- **Operazioni supportate**:
+  - Addizione (+)
+  - Sottrazione (-)
+  - Moltiplicazione (*)
+  - Divisione (/)
--- obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2125dba1607c93696871e2e7e2679310feb9655")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
